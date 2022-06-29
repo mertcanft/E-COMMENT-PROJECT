e63e5dc07be0a7278fba126203b5a4fcd98c889e
mertcanft <mertcanft@gmail.com>
2022-06-29 10:42:57 +0300

    Add files via upload

---
+++ E-COMMENT-PROJECT/Filter/products.js
@@ -0,0 +1,89 @@
+const products = [
+  {
+    id: "9",
+    title:
+      "ProJcb Plus X-TORQ JSR 3650 Lİ SJS Çift Metal Şanzuman Çift Akülü Şarjlı Matkap Set Hediyeli",
+    company: "Tamir, Tadilat Gereçleri",
+    image: "https://mcdn01.gittigidiyor.net/60945/tn50/609452818_tn50_1.jpg",
+    price: 199.9,
+  },
+  {
+    id: "10",
+    title: "Bosch X-Line 38 Parça Aksesuar Set - 2607011432",
+    company: "Tamir, Tadilat Gereçleri",
+    image: "https://mcdn01.gittigidiyor.net/68404/tn50/684043005_tn50_0.jpg",
+    price: 94.9,
+  },
+  {
+    id: "11",
+    title: "OckGarden's Metal Salıncak Bahçe Teras Balkon Salıncağı",
+    company: "Mobilya",
+    image: "https://mcdn01.gittigidiyor.net/65019/tn50/650190099_tn50_0.jpg",
+    price: 1099.0,
+  },
+  {
+    id: "12",
+    title: "PORMET Çift Yandan Raflı Bez Dolap - Siyah",
+    company: "Mobilya",
+    image: "https://mcdn01.gittigidiyor.net/61192/tn50/611925118_tn50_0.jpg",
+    price: 123.94,
+  },
+  {
+    id: "13",
+    title: "Beurer BM 27 Tansiyon Aleti",
+    company: "Sağlık, Medikal",
+    image: "https://mcdn01.gittigidiyor.net/62299/tn50/622994093_tn50_0.jpg",
+    price: 175.99,
+  },
+  {
+    id: "14",
+    title:
+      "Are 3 Katlı Meltblown Ultrasonik 50'li 3 Paket Telli Cerrahi Yüz Maskesi",
+    company: "Sağlık, Medikal",
+    image: "https://mcdn01.gittigidiyor.net/63730/tn50/637309846_tn50_0.jpg",
+    price: 97.9,
+  },
+  {
+    id: "15",
+    title: "Canped Emici Külot Büyük Boy Large 60 Adet",
+    company: "Sağlık, Medikal",
+    image: "https://mcdn01.gittigidiyor.net/64996/tn50/649968407_tn50_0.jpg",
+    price: 282.95,
+  },
+  {
+    id: "16",
+    title: "MSI GF75 Thin 10SC-003XTR Dizüstü Bilgisayar (Laptop)",
+    company: "Dizüstü Bilgisayar (Laptop)",
+    image: "https://mcdn01.gittigidiyor.net/68342/tn50/683423941_tn50_0.jpg",
+    price: 12744.0,
+  },
+  {
+    id: "17",
+    title:
+      "Asus D515DA-BR608 AMD Ryzen 3 3250U 4GB 256GB SSD FDOS 15.6 inc Taşınabilir Bilgisayar",
+    company: "Dizüstü Bilgisayar (Laptop)",
+    image: "https://mcdn01.gittigidiyor.net/70356/tn50/703564485_tn50_0.jpg",
+    price: 3699.0,
+  },
+  {
+    id: "18",
+    title: "MSI Modern 14 B10MW-271XTR Dizüstü Bilgisayar (Laptop)",
+    company: "Dizüstü Bilgisayar (Laptop)",
+    image: "https://mcdn01.gittigidiyor.net/67229/tn50/672296793_tn50_0.jpg",
+    price: 8462.0,
+  },
+  {
+    id: "19",
+    title: "Baymak Lambert Attivo 24 LN Kombi",
+    company: "Isıtma, Soğutma Sistemi",
+    image: "https://mcdn01.gittigidiyor.net/59238/tn50/592383220_tn50_0.jpg",
+    price: 3849.0,
+  },
+  {
+    id: "20",
+    title: "Demirdöküm Kion 12 Klima",
+    company: "Isıtma, Soğutma Sistemi",
+    image: "https://mcdn01.gittigidiyor.net/56525/tn50/565250221_tn50_0.jpg",
+    price: 4070.0,
+  },
+];
---
+++ E-COMMENT-PROJECT/README.md
@@ -0,0 +1,12 @@
+# E-Comment-Project
+E-Comment-Project
+
+Demo Link : https://e-comment-home.netlify.app
+
+![img 1](https://user-images.githubusercontent.com/96665012/165855551-4336ea48-a8c5-4d61-b22a-d81046e3023d.PNG)
+![img 2](https://user-images.githubusercontent.com/96665012/165855584-be544a1d-e76d-4cbf-aa29-ca27d4a34eb5.PNG)
+![img3](https://user-images.githubusercontent.com/96665012/165855614-da0ccdea-768e-4319-9c27-01b585b1c76a.PNG)
+![img 4](https://user-images.githubusercontent.com/96665012/165855621-4a0f0ef6-3b76-4569-bcbd-6d980aa9e05f.PNG)
+![img 5](https://user-images.githubusercontent.com/96665012/165855628-8de1f550-4ede-4225-b636-7a7f6226cfab.PNG)
+![img 6](https://user-images.githubusercontent.com/96665012/165855635-009361d0-5f94-4f82-b6e7-eb326ee81a49.PNG)
+
